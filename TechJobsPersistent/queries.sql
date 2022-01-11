85d39d09b49a81823bfd2286e0dcbb006852dc93
Dan Cowdry <medtc411@gmail.com>
2022-01-11 14:34:32 -0800

    SQL

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,14 @@
 --Part 1
-
+SELECT COLUMN_NAME, DATA_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'jobs'; 
 --Part 2
-
+SELECT Name 
+FROM employers 
+WHERE Location = 'Saint Louis';
 --Part 3
-
+SELECT skills.Name,skills.Description 
+FROM jobskills 
+LEFT JOIN skills ON skills.id = jobskills.skillid 
+WHERE techjobs.jobSkills.jobId IS NOT NULL
+ORDER BY skills.Name ASC;
